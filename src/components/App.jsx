db73d801404d7709a0db11f4bf0800bbe9101744
BulatovaTati <kohodatat@gmail.com>
2022-10-27 22:04:42 +0300

    add test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import Profile from './userInfo/user';
+import user from './userInfo/user.json';
+ 
+import Statistics from './statistics/statistics';
+import statisticalData from './statistics/data.json';
+import FriendList from './friends/friends';
+import friends from './friends/friends.json';
+import TransactionHistory from './transactions/transactions';
+import transactions from './transactions/transactions.json';
+
+  function RenderInPage() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+    <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+ <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+     <FriendList friends={friends} />
+ <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
+export default RenderInPage;
+ 
