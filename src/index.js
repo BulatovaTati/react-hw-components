79784cb6217c42d37802e8dfd831a191c4bb60cc
BulatovaTati <kohodatat@gmail.com>
2022-11-10 23:12:55 +0200

    add normalise

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'components/theme';
 import './index.css';
+import 'modern-normalize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
