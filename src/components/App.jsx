b2cf72ab7e35d966836856f57631dec580580b32
BulatovaTati <kohodatat@gmail.com>
2022-11-14 16:55:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FeedbackPage from '../pages/Feedback';
+import FeedbackPage from 'pages/Feedback';
 import Navbar from './NavBar/NavBar';
-import Phonebook from '../pages/Phonebook';
-import Home from '../pages/Home';
+import Phonebook from 'pages/Phonebook';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Feedback" element={<FeedbackPage />} />
-        <Route path="/Phonebook" element={<Phonebook />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/phonebook" element={<Phonebook />} />
       </Routes>
     </Router>
   );
