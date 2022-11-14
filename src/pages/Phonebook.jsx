d8114f7b0a873f54b6a17561351de1c4d440a67d
BulatovaTati <kohodatat@gmail.com>
2022-11-14 16:43:18 +0200

    Add localStorage save

--- src/pages/Phonebook.jsx
+++ src/pages/Phonebook.jsx
@@ -10,12 +10,7 @@ import {
 
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     isOpenForm: false,
     isOpenFilter: false,
@@ -50,6 +45,19 @@ class Phonebook extends Component {
       [component]: !prevState[component],
     }));
   };
+
+  componentDidMount() {
+    const getContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (getContacts) {
+      this.setState({ contacts: getContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter, contacts, isOpenForm, isOpenFilter } = this.state;
