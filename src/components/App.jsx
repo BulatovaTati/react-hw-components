bf78903ea61979ef0e2930f5af42fdce66630bdc
BulatovaTati <kohodatat@gmail.com>
2022-11-14 17:03:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
         <Route path="/feedback" element={<FeedbackPage />} />
         <Route path="/phonebook" element={<Phonebook />} />
       </Routes>
