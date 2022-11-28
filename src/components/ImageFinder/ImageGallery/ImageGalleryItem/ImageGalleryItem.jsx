d60403a357a977d911cf9220e9bf7b2f589818d6
BulatovaTati <kohodatat@gmail.com>
2022-11-28 16:14:45 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageFinder/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageFinder/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,21 +1,21 @@
 import propTypes from 'prop-types';
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 
-const ImageGalleryItem = ({ url, largeImageURL, tags, openModal }) => (
+const ImageGalleryItem = ({ url, largeUrl, tags, onOpenModal }) => (
   <GalleryItem>
     <GalleryImage
       src={url}
       alt={tags}
-      onClick={() => openModal(largeImageURL, tags)}
+      onClick={() => onOpenModal(largeUrl, tags)}
     />
   </GalleryItem>
 );
 
 ImageGalleryItem.propTypes = {
   url: propTypes.string.isRequired,
-  largeImageURL: propTypes.string.isRequired,
+  largeUrl: propTypes.string.isRequired,
   tags: propTypes.string.isRequired,
-  openModal: propTypes.func.isRequired,
+  onOpenModal: propTypes.func.isRequired,
 };
 
 export default ImageGalleryItem;
