5cba0b174826de3f36034c166a68d9701ef9f150
BulatovaTati <kohodatat@gmail.com>
2022-11-28 12:51:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/">
       <ThemeProvider theme={theme}>
         <App />
-      </ThemeProvider>{' '}
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
